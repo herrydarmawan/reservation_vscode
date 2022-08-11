90b571c64c5b9368d93fa6e2aa286ed9b8a1e468
herrydarmawan <hdarmawan123@gmail.com>
2022-08-11 21:47:55 +0700

    Update Homejsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route/>
+        <Route path='' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
