606bc817920b62ab72bca1ca3588b539eb243672
herrydarmawan <hdarmawan123@gmail.com>
2022-08-12 15:54:05 +0700

    new update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='' element={<Home/>}/>
+        <Route path='/hotels' element={<List/>}/>
+        <Route path='/hotels/:id' element={<Hotel/>}/>
       </Routes>
     </BrowserRouter>
   );
